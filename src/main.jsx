df96785485d873e2db2c012c433ee3dd5898a4b4
João Inácio Neto <birobirobiro.dev@gmail.com>
2022-03-21 09:45:26 -0300

    projeto finalizado

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Home } from "./pages/Home";
+import "./styles/global.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
